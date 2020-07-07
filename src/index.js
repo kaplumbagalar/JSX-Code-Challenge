9d039024fdf601cee1a720b691c08093f75d5aed
kaplumbagalar <56247924+kaplumbagalar@users.noreply.github.com>
2020-07-07 12:08:25 +0300

    Challenge completed.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+ReactDom.render(
+  <div>
+    <h1>This is an unordered list.</h1>
+    <ul>
+      <li> Banana </li>
+      <li> Apple </li>
+      <li> Cherry </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
